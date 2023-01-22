2d63423a71ea532cbc3937c931b02cdbb07c597a
Anurodh Singh <anurodhsinghmp@gmail.com>
2023-01-21 16:40:39 -0800

    linkedlist

--- linked_list.cpp
+++ linked_list.cpp
@@ -54,6 +54,7 @@ void display(Node *head)
 //reverse a linked list
 Node* reverse(Node *head, Node *&tail)
 {
+    if(head== NULL) return head;
     Node *current= head, *prev= NULL, *nextnode= NULL;
     while(current!= NULL)
     {
@@ -62,11 +63,14 @@ Node* reverse(Node *head, Node *&tail)
         prev= current;
         current= nextnode;
     }
+    tail= head;
     return prev;
 }
 //making cyclic linked list
 Node* makecycliclinkedlist(Node *head, Node *&tail, int pos)
 {
+    if(head == NULL)
+    return head;
     Node *temp= head;
     int i=1;
     while(i<pos)
@@ -97,6 +101,7 @@ pair<bool, Node*> detectloop(Node *head, Node *&tail)
 //to find the position where the cycle of linked list start
 Node* cycleposition(Node *head, Node *&tail)
 {
+    if(head == NULL) return head;
     pair<bool, Node*> p= detectloop(head, tail);
     if(p.first)
     {
@@ -115,6 +120,7 @@ Node* cycleposition(Node *head, Node *&tail)
 Node* removecycle(Node *head, Node *&tail)
 {
     //starting pos of cycle
+    if(head== NULL) return head;
     Node *startingpos= cycleposition(head, tail);
     Node *temp= startingpos;
     while(temp->next!= startingpos)
@@ -124,6 +130,115 @@ Node* removecycle(Node *head, Node *&tail)
     temp->next= NULL;
     tail= temp;
     return head;
+}
+//remove the duplicates from the linked list
+Node* removeduplicate(Node *head, Node *&tail)
+{   
+    if(head == NULL)
+    {
+        return head;
+    }
+    Node *temp= head;
+    while(temp->next!=NULL)
+    {
+            if(temp->next->data == temp->data)
+            {
+                temp->next= temp->next->next;
+            }
+            else{
+                temp= temp->next;
+            }
+    }
+    return head;
+}
+
+//remove the duplicates from the unsorted linked list
+Node* removeduplicateunsorted(Node *head, Node *&tail)
+{
+    if(head== NULL)
+    return head;
+    Node *t=NULL;
+    unordered_set<int> st;
+    Node *temp= head;
+    while(temp!=NULL)
+    {
+        if(st.find(temp->data)!=st.end())
+        {
+            t->next= temp->next;
+        }
+        else{
+            st.insert(temp->data);
+            t= temp;
+        }
+        temp= temp->next;
+    }
+    return head;
+    
+}
+//move the last node to first
+Node* movelasttofirst(Node *head, Node *&tail)
+{
+    if(head == NULL) return head;
+    Node *temp= head;
+    while(temp->next!=tail)
+    {
+        temp= temp->next;
+    }
+    temp->next= NULL;
+    tail->next= head;
+    head= tail;
+    tail= temp;
+    return head;
+}
+
+//adding 1 to linked list
+Node *addOne(Node* head, Node *&tail)
+{
+    if(head== NULL) return head;
+    head= reverse(head, tail);
+    Node *temp= head;
+    int carry= 1;
+    Node *t=temp;
+    while(temp!=NULL || carry > 0)
+    {
+        int value= carry;
+        if(temp!=NULL)
+        {
+            value += temp->data;
+        }
+        carry= value/10;
+        value= value%10;
+        if(temp!=NULL)
+        {
+        temp->data= value;
+        }
+        else
+        {
+            Node *newnode= new Node(value);
+            t->next= newnode;
+            t= newnode;
+            tail= newnode;
+        }
+        if(temp!=NULL)
+        {
+        t = temp;
+        temp= temp->next;
+        }
+    }
+    head= reverse(head, tail);
+    return head;
+}
+//middle element
+int middlenode(Node *head)
+{
+    if(head==NULL) return -1;
+    Node *fast= head, *slow= head;
+    while(fast!=NULL && fast->next != NULL)
+    {
+        fast = fast->next->next;
+        slow= slow->next;
+    }
+    return slow->data;
 }
 int main()
 {
@@ -140,6 +255,11 @@ int main()
     cout<<"5. to detect whether the cycle present in linked list or not :"<<endl;
     cout<<"6. to find the starting point of cycle: "<<endl;
     cout<<"7. to remove the cycle from the linked list :"<<endl;
+    cout<<"8. to remove the duplicate from sorted linked list: "<<endl;
+    cout<<"9. to remove the duplicate from the unsorted linked list: "<<endl;
+    cout<<"10. to move the last node to first pos: "<<endl;
+    cout<<"11. to add 1 in  linked list: "<<endl;
+    cout<<"12. to find the middle element of linked list: "<<endl;
     cout<<"0. to exit"<<endl;
     cout<<"\n enter your choice: ";
     cin>>choice;
@@ -179,6 +299,31 @@ int main()
     case 7:
         head= removecycle(head, tail);
         break;
+    case 8:
+        head= removeduplicate(head, tail);
+        display(head);
+        break;
+    case 9:
+        head= removeduplicateunsorted(head, tail);
+        display(head);
+        break;
+    case 10:
+        head= movelasttofirst(head, tail);
+        display(head);
+        break;
+    case 11:
+        head= addOne(head, tail);
+        display(head);
+        break;
+    case 12:
+        {
+            int middle= middlenode(head);
+            if(middle== -1)
+            cout<<"middle element not found"<<endl;
+            else
+            cout<<"Middle element of the linked list: "<<middle<<endl;
+            break;
+        }
     default: 
         cout<<"Please enter a valid option!!"<<endl;
         break;
